4fa0fbc24553e65deee9bb4f1a46897717f80a1f
赵昊 <zhaohao.9426@bytedance.com>
2023-06-08 19:39:54 +0800

    add transaction test

--- transaction_test.go
+++ transaction_test.go
@@ -1,1 +1,56 @@
 package simple_orm
+
+import (
+	"context"
+	"database/sql"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTx_Commit(t *testing.T) {
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = mockDB.Close() }()
+
+	db, err := OpenDB(mockDB)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		mock.ExpectClose()
+	}()
+
+	// 事务正常提交
+	mock.ExpectBegin()
+	mock.ExpectCommit()
+
+	tx, err := db.beginTx(context.Background(), &sql.TxOptions{})
+	assert.Nil(t, err)
+	err = tx.Commit()
+	assert.Nil(t, err)
+
+}
+
+func TestTx_Rollback(t *testing.T) {
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = mockDB.Close() }()
+
+	db, err := OpenDB(mockDB)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 事务回滚
+	mock.ExpectBegin()
+	mock.ExpectRollback()
+	tx, err := db.beginTx(context.Background(), &sql.TxOptions{})
+	assert.Nil(t, err)
+	err = tx.Rollback()
+	assert.Nil(t, err)
+}
